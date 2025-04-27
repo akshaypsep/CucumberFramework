b1e73e720f95aff7afaf8ee5820cde54e2727f15
ractiwar <tiwari.rachna@gmail.com>
2025-04-27 09:23:51 +0530

    added a data driven scenario to leads

--- src/test/java/com/vtiger/stepdefinitions/leadsteps.java
+++ src/test/java/com/vtiger/stepdefinitions/leadsteps.java
@@ -31,4 +31,23 @@ public class leadsteps extends basesteps {
              }
     }
 
+    @When("User need to create multiple leads and validate based on data")
+    public void user_need_to_create_multiple_leads_and_validate_based_on_data(io.cucumber.datatable.DataTable dataTable) {
+        List<List<String>> ls = dataTable.asLists();
+        for (int i = 0; i < ls.size(); i++) {
+            driver.findElement(By.xpath("//a[contains(text(),'New Lead')]")).click();
+            // wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name = 'lastname']")));
+            driver.findElement(By.xpath("//input[@name = 'lastname']")).sendKeys(ls.get(i).get(0));
+            driver.findElement(By.xpath("//input[@name = 'company']")).sendKeys(ls.get(i).get(1));
+            driver.findElement(By.name("button")).click();
+            driver.findElement(By.xpath("//td[text()='Last Name:']/following-sibling::td[1][text()='" + ls.get(i).get(0) + "']")).isDisplayed();
+            driver.findElement(By.xpath("//td[text()='Company:']/following-sibling::td[1][text()='" + ls.get(i).get(1) + "']")).isDisplayed();
+        }
+
+    }
+
+    @When("Close Browser")
+    public void close_browser() {
+        driver.close();
+    }
 }
